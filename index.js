5ca090188f383ed5bfd20f199c106d6211dfdb06
Vedant Seta <vedant.s@media.net>
2017-06-17 22:42:01 +0530

    refactor

--- index.js
+++ index.js
@@ -1,69 +1,207 @@
 const request = require("sync-request");
+const inquirer = require("inquirer");
+const { log } = console;
 
-const COOKIE_STRING = "";
-const GET_VIDEO = "http://www.youlikehits.com/youtubenew2.php?step=reload&rand=0.03451081519794963";
-const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
-const seconds = 15;
+const COOKIE_STRING =
+	"";
+const CAPTCHA_ANSWER = 4;
+
+const GET_VIDEO =
+	"http://www.youlikehits.com/youtubenew2.php?step=reload&rand=0.1";
+const USER_AGENT =
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+
+const CAPTCHA_CHECKER = "Solve the Problem and Submit";
+const LOGIN_CHECKER = "Please login again";
+const CAPTCHA_RETRY_CHECKER = "You did not successfully solve the problem.";
+const VIEW_LENGTH_CHECKER = "You didn't view the video for the specified length of time";
+const HOST = "http://www.youlikehits.com/";
+
 const options = {
-    headers: {
-    	'Cookie': COOKIE_STRING,
-    	'User-Agent' : USER_AGENT
+	headers: {
+		Cookie: COOKIE_STRING,
+		"User-Agent": USER_AGENT
+	},
+	retry: true,
+	maxRetries: 3
+};
+
+const API = {
+	youtube: {
+		GET: {
+			url: `${HOST}youtubenew2.php?step=reload&rand=0.1`,
+			method: "GET"
+		},
+		CAPTCHA: {
+			url: `${HOST}youtubenew2.php`,
+			method: "POST"
+		}
 	}
 };
+
+const checkers = {
+	captcha_checker: function(response) {
+		if (response.includes(CAPTCHA_CHECKER)) {
+			solveCaptcha();
+		}
+		return true;
+	},
+	login_checker: function(response) {
+		if (response.includes(LOGIN_CHECKER)) {
+			exit("LOGIN IS REQUIRED");
+		}
+		return true;
+	},
+	captcha_rety_checher: function(response) {
+		if (response.includes(CAPTCHA_RETRY_CHECKER)) {
+			return false;
+		}
+		return true;
+	},
+	view_length_checker : function(response) {
+		if(response.includes(VIEW_LENGTH_CHECKER)) {
+			return false;
+		}
+		return true;
+	}
+};
+
+const exit = function(message) {
+	log(message);
+	process.exit(0);
+};
+
+const wait = function (seconds = 1) {
+	let waitTill = new Date(new Date().getTime() + seconds * 1000);
+	while (waitTill > new Date()) {}
+	return true;
+};
+
+const solveCaptcha = function(times = 3) {
+	let option = Object.assign(options);
+
+	option["headers"]["Content-Type"] = "application/x-www-form-urlencoded";
+	option["body"] = `answer=${CAPTCHA_ANSWER}&submit=Submit`;
+
+	let response = requestURL(
+		API["youtube"]["CAPTCHA"]["method"],
+		API["youtube"]["CAPTCHA"]["url"],
+		options,
+		[checkers["captcha_rety_checher"]]
+	);
+
+	if (response.includes(CAPTCHA_RETRY_CHECKER) && times > 0) {
+		return solveCaptcha(--times);
+	}
+};
+
+
+const requestURL = function(method, URL, options, checkers = [], times = 3) {
+	if (times === 0) {
+		exit("URL REQUEST PROBLEM" + URL);
+	}
+	let response,
+		passed = true;
+	try {
+		response = request(method, URL, options);
+		response = response.getBody("utf8");
+		let passed = checkers.reduce(function(result, currentFunction) {
+			return result && currentFunction(response);
+		}, true);
+	} catch (e) {
+		passed = false;
+		log(e);
+	}
+	response = passed
+		? response
+		: requestURL(method, URL, options, checkers, --times);
+	return response;
+}
+
+
+// const questions = [
+// 	{
+// 		type: "confirm",
+// 		name: "login",
+// 		message: "Did you login on youlikehits.com?",
+// 		default: false
+// 	},
+// 	{
+// 		type: "input",
+// 		name: "cookie",
+// 		message: "Please Copy Paste the Cookie you get on login",
+// 		default: false,
+// 		when: (answers) => {
+// 			if(!answers.login) {
+// 				log("You must login");
+// 				return false;
+// 			}
+// 			return true;
+// 		}
+// 	},
+// 	{
+// 		type: "input",
+// 		name: "captcha",
+// 		message: `I promise.. this is last step ..Go to view youtube and solve captcha and input result here`,
+// 		default: false,
+// 		when: (answers) => {
+// 			if(!answers.cookie) {
+// 				log("You must enter your cookie first");
+// 				return false;
+// 			}
+// 			return true;
+// 		}
+// 	},
+// ];
+// inquirer.prompt(questions).then(function(answers) {
+// 	console.log(answers);
+// });
+
 let count = 0;
 let counter = 0;
-while(count < 10) {
-	counter++;
-	console.log('Counter'+counter);
-	let response;
-	try {
-		response = request('GET', GET_VIDEO, options);
-		response = response.getBody('utf8');
-	} catch(e) {
-		console.log(e);
-		continue;
-	}	
 
-	console.log(response);
-	if(response.includes("Solve the Problem and Submit")) {
-		console.log('continue');
-		continue;
-	}
+while (count < 10) {
+	counter++;
+	console.log("Counter" + counter);
+	let response;
+	let defaultCheckers = [
+		checkers["captcha_checker"],
+		checkers["login_checker"],
+		checkers["view_length_checker"]
+	];
+
+	response = requestURL(
+		API["youtube"]["GET"]["method"],
+		API["youtube"]["GET"]["url"],
+		options,
+		defaultCheckers
+	);
+
 	let detailsStart = response.indexOf("imageWin") + "imageWin(".length;
 	let detailsEnd = response.indexOf(");", detailsStart);
 	let details = response.substring(detailsStart, detailsEnd);
 	details = details.split(",").map(function(element) {
 		return element.trim().replace(/'/g, "");
-	})
-	console.log(details);
-	if(details.length !== 4) {
-		continue;
-		process.exit(0);
-	}
-	
-	let  VIEW_VIDEO = `http://www.youlikehits.com/youtuberender.php?id=${details[0]}&step=points&x=${details[3]}&rand=0.04596779850586352`;
-	try {
-		response = request('GET', VIEW_VIDEO, options);		
-	} catch (e) {
-		console.log(e);
+	});
+	if (details.length !== 4) {
+		console.log("Skipped a video");
 		continue;
 	}
-	console.log('PASSED');
-	var waitTill = new Date(new Date().getTime() + parseInt(details[2]) * 1000);
-	while(waitTill > new Date()){}
-	console.log('Wait complete');
+	log("Got a video :D");
+	let VIEW_VIDEO = `http://www.youlikehits.com/youtuberender.php?id=${details[0]}&step=points&x=${details[3]}&rand=0.04596779850586352`;
+	response = requestURL("GET", VIEW_VIDEO, options, defaultCheckers);
 
+	// WAIT
+	let seconds = parseInt(details[2]) + 5;
+	log(`Waiting for ${seconds} seconds`);
+	let waiting = wait(seconds);
+
+	// GET POINTS
 	const GET_POINTS = `http://www.youlikehits.com/playyoutubenew.php?id=${details[0]}&step=points&x=${details[3]}&rand=0.04596779850586352`;
-	console.log(GET_POINTS);
-	try {
-		response = request('GET', GET_POINTS, options);
-	} catch(e) {
-		console.log(e);
-		continue;
-	}
-	response = response.getBody('utf8');
-	console.log(response);
-	console.log(count);
-	count++;
+	
+	response = requestURL("GET", GET_POINTS, options, defaultCheckers);
+	log("Get Points");
+	log(response);
 }
+
 
